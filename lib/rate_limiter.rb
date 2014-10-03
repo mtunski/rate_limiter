577e76b62198982941ee4dbd0886cce5aaf48d35
Mateusz Tuński <mateusz@tunski.net>
2014-10-03 14:15:57 +0200

    [#2] Implement the functionality of the middleware to add custom header

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -1,4 +1,15 @@
 require 'rate_limiter/version'
 
 class RateLimiter
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    @status, @headers, @response = @app.call(env)
+
+    @headers['X-RateLimit-Limit'] = 60
+
+    [@status, @headers, @response]
+  end
 end
