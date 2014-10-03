90b8bcd5eaf837fd40c2106d6d0666d876e726ff
Mateusz Tuński <mateusz@tunski.net>
2014-10-03 18:49:57 +0200

    [#2] Apply improvements according to Trello comments

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -4,13 +4,14 @@ class RateLimiter
   def initialize(app, options={})
     @app     = app
     @options = options
+    @options['limit'] ||= 60
   end
 
   def call(env)
-    @status, @headers, @response = @app.call(env)
+    status, headers, response = @app.call(env)
 
-    @headers['X-RateLimit-Limit'] = @options['limit'] ||= 60
+    headers['X-RateLimit-Limit'] = @options['limit']
 
-    [@status, @headers, @response]
+    [status, headers, response]
   end
 end
