1b01e184a0333066a509b6942d87f9ca4296299d
Mateusz Tuński <mateusz@tunski.net>
2014-10-03 13:39:39 +0200

    [#1] Simplify the first test

--- test/test_rate_limiter.rb
+++ test/test_rate_limiter.rb
@@ -4,12 +4,13 @@ class RateLimiterTest < Minitest::Test
   include Rack::Test::Methods
 
   def app
-    lambda { |env| ['200', {'Content-Type' => 'text/html'}, ['App']] }
+    lambda { |env| [200, {}, 'App'] }
   end
 
   def test_server_responds_successfully
     get '/'
 
     assert last_response.ok?
+    assert_equal 'App', last_response.body
   end
 end
