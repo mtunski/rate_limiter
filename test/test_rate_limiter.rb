49909700fa49debc173d9a5a21b4bd4180518983
Mateusz Tuński <mateusz@tunski.net>
2014-10-03 18:50:42 +0200

    Perform 'get' request to rack server in tests setup

--- test/test_rate_limiter.rb
+++ test/test_rate_limiter.rb
@@ -4,6 +4,10 @@ require 'rate_limiter'
 class RateLimiterTest < Minitest::Test
   include Rack::Test::Methods
 
+  def setup
+    get '/'
+  end
+
   def app
     app     = lambda { |env| [200, {}, 'App'] }
     options = { 'limit' => 30 }
@@ -12,23 +16,17 @@ class RateLimiterTest < Minitest::Test
   end
 
   def test_server_responds_successfully
-    get '/'
-
     assert last_response.ok?
     assert_equal 'App', last_response.body
   end
 
   def test_middleware_adds_desired_header
-    get '/'
-
     headers = last_response.headers
 
     assert headers.has_key?('X-RateLimit-Limit')
   end
 
   def test_middleware_handles_options_param_with_limit_value
-    get '/'
-
     assert_equal 30, last_response.headers['X-RateLimit-Limit']
   end
 end
