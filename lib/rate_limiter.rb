c6cd894809c72a98bfa1c5687a825c0462f88843
Mateusz Tuński <mateusz@tunski.net>
2014-10-03 14:46:00 +0200

    [#2] Implement the functionality of handling options param by middleware

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -1,14 +1,15 @@
 require 'rate_limiter/version'
 
 class RateLimiter
-  def initialize(app)
-    @app = app
+  def initialize(app, options={})
+    @app     = app
+    @options = options
   end
 
   def call(env)
     @status, @headers, @response = @app.call(env)
 
-    @headers['X-RateLimit-Limit'] = 60
+    @headers['X-RateLimit-Limit'] = @options['limit'] ||= 60
 
     [@status, @headers, @response]
   end
