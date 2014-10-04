2e07ab56888cb1490e1bc07ee27569e5c1ceb8b6
Mateusz Tuński <mateusz@tunski.net>
2014-10-04 12:17:34 +0200

    [#5] Implement the functionality of resetting the limit of requests after 1h

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -8,12 +8,18 @@ class RateLimiter
   end
 
   def call(env)
+    if @reset && (@reset <= Time.now.to_i)
+      @reset     = (Time.now + 60*60).to_i
+      @remaining = @options['limit']
+    end
+
     return [429, {}, 'Too many requests'] if @remaining == 0
 
     status, headers, response = @app.call(env)
 
     headers['X-RateLimit-Limit']     = @options['limit']
     headers['X-RateLimit-Remaining'] = (@remaining -= 1)
+    headers['X-RateLimit-Reset']     = (@reset ||= (Time.now + 60*60).to_i)
 
     [status, headers, response]
   end
