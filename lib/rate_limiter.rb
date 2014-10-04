db1276fa02d68255d1a4db59fba3b3664f331961
Mateusz Tuński <mateusz@tunski.net>
2014-10-04 14:07:25 +0200

    [#6] Implement the functionality of separating limits for each client

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -2,41 +2,53 @@ require 'rate_limiter/version'
 
 class RateLimiter
   def initialize(app, options = {})
-    @app       = app
-    @options   = options
-    @remaining = (@options['limit']    ||= 60)
-    @reset_in  = (@options['reset_in'] ||= 3600)
+    @app      = app
+    @options  = options
+    @reset_in = (@options['reset_in'] ||= 3600)
+    @clients  = {}
   end
 
   def call(env)
-    reset_limit               if should_reset?
-    return limit_hit_response if limit_hit?
+    ip = env['REMOTE_ADDR']
+
+    setup_client(ip)          unless client_registered?(ip)
+    reset_limit(ip)           if should_reset?(ip)
+    return limit_hit_response if limit_hit?(ip)
 
     status, headers, response = @app.call(env)
 
     headers['X-RateLimit-Limit']     = @options['limit']
-    headers['X-RateLimit-Remaining'] = (@remaining -= 1)
-    headers['X-RateLimit-Reset']     = (@reset ||= (Time.now + @reset_in).to_i)
+    headers['X-RateLimit-Remaining'] = (@clients[ip]['remaining'] -= 1)
+    headers['X-RateLimit-Reset']     = (@clients[ip]['reset'] ||= (Time.now + @reset_in).to_i)
 
     [status, headers, response]
   end
 
   private
 
-  def limit_hit?
-    @remaining == 0
+  def setup_client(ip)
+    @clients[ip]              = {}
+    @clients[ip]['remaining'] = @options['limit']
+  end
+
+  def client_registered?(ip)
+    @clients.has_key?(ip)
+  end
+
+  def limit_hit?(ip)
+    @clients[ip]['remaining'] == 0
   end
 
   def limit_hit_response
     return [429, {}, 'Too many requests']
   end
 
-  def should_reset?
-    @reset && (@reset <= Time.now.to_i)
+  def should_reset?(ip)
+    @clients[ip]['reset'] && (@clients[ip]['reset'] <= Time.now.to_i)
   end
 
-  def reset_limit
-    @reset     = (Time.now + @reset_in).to_i
-    @remaining = @options['limit']
+  def reset_limit(ip)
+    @clients[ip]['reset']     = (Time.now + @reset_in).to_i
+    @clients[ip]['remaining'] = @options['limit']
   end
 end
