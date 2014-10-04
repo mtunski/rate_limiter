95a3acca56c46d7f5331ec6fd680828cdf61592e
Mateusz Tuński <mateusz@tunski.net>
2014-10-04 12:20:43 +0200

    Styling fixes - use spaces around operators

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -1,7 +1,7 @@
 require 'rate_limiter/version'
 
 class RateLimiter
-  def initialize(app, options={})
+  def initialize(app, options = {})
     @app       = app
     @options   = options
     @remaining = (@options['limit'] ||= 60)
@@ -9,7 +9,7 @@ class RateLimiter
 
   def call(env)
     if @reset && (@reset <= Time.now.to_i)
-      @reset     = (Time.now + 60*60).to_i
+      @reset     = (Time.now + 60 * 60).to_i
       @remaining = @options['limit']
     end
 
@@ -19,7 +19,7 @@ class RateLimiter
 
     headers['X-RateLimit-Limit']     = @options['limit']
     headers['X-RateLimit-Remaining'] = (@remaining -= 1)
-    headers['X-RateLimit-Reset']     = (@reset ||= (Time.now + 60*60).to_i)
+    headers['X-RateLimit-Reset']     = (@reset ||= (Time.now + 60 * 60).to_i)
 
     [status, headers, response]
   end
