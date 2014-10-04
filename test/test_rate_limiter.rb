c3b981ea225bb3d10a8d9c4743dfa41d09ceb679
Mateusz Tuński <mateusz@tunski.net>
2014-10-04 12:15:00 +0200

    [#5] Implement tests for resetting the limit of requests after 1 hour

--- test/test_rate_limiter.rb
+++ test/test_rate_limiter.rb
@@ -52,4 +52,22 @@ class RateLimiterTest < Minitest::Test
     assert_equal 'Too many requests', last_response.body
     refute last_response.headers.has_key?('Test-Header')
   end
+
+  def test_middleware_adds_ratelimit_reset_header
+    assert last_response.headers.has_key?('X-RateLimit-Reset')
+    assert_in_delta (Time.now + 60*60).to_i, last_response.headers['X-RateLimit-Reset'], 5
+  end
+
+  def test_middleware_handles_resetting_the_limit_of_remaining_requests_and_time_for_next_reset
+    4.times { get('/') }
+
+    assert_equal 25, last_response.headers['X-RateLimit-Remaining']
+
+    Timecop.freeze(Time.now + 61*60) do
+      get('/')
+
+      assert_equal 29, last_response.headers['X-RateLimit-Remaining']
+      assert_in_delta (Time.now + 60*60).to_i, last_response.headers['X-RateLimit-Reset'], 5
+    end
+  end
 end
