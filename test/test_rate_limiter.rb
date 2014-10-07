69dca955557111dc4793aa833366eba5dc7eb01d
Mateusz Tuński <mateusz@tunski.net>
2014-10-07 12:20:46 +0200

    [#8] Fix tests (headers returned as strings)

--- test/test_rate_limiter.rb
+++ test/test_rate_limiter.rb
@@ -31,7 +31,7 @@ class RateLimiterTest < Minitest::Test
   def test_middleware_handles_options_param_with_limit_value
     get '/'
 
-    assert_equal 30, last_response.headers['X-RateLimit-Limit']
+    assert_equal 30, last_response.headers['X-RateLimit-Limit'].to_i
   end
 
   def test_middleware_adds_ratelimit_remaining_header
@@ -43,17 +43,17 @@ class RateLimiterTest < Minitest::Test
   def test_middleware_handles_decreasing_the_number_of_remaining_requests
     get '/'
 
-    assert_equal 29, last_response.headers['X-RateLimit-Remaining']
+    assert_equal 29, last_response.headers['X-RateLimit-Remaining'].to_i
 
     4.times { get('/') }
 
-    assert_equal 25, last_response.headers['X-RateLimit-Remaining']
+    assert_equal 25, last_response.headers['X-RateLimit-Remaining'].to_i
   end
 
   def test_middleware_handles_limiting_number_of_requests
     30.times { get('/') }
 
-    assert_equal 0, last_response.headers['X-RateLimit-Remaining']
+    assert_equal 0, last_response.headers['X-RateLimit-Remaining'].to_i
     assert last_response.headers.has_key?('Test-Header')
 
     get('/')
@@ -67,30 +67,30 @@ class RateLimiterTest < Minitest::Test
     get '/'
 
     assert last_response.headers.has_key?('X-RateLimit-Reset')
-    assert_in_delta (Time.now + 2 * 60 * 60).to_i, last_response.headers['X-RateLimit-Reset'], 5
+    assert_in_delta (Time.now + 2 * 60 * 60).to_i, last_response.headers['X-RateLimit-Reset'].to_i, 5
   end
 
   def test_middleware_handles_resetting_the_limit_of_remaining_requests_and_time_for_next_reset
     5.times { get('/') }
 
-    assert_equal 25, last_response.headers['X-RateLimit-Remaining']
+    assert_equal 25, last_response.headers['X-RateLimit-Remaining'].to_i
 
     Timecop.freeze(Time.now + 2 * 61 * 60) do
       get('/')
 
-      assert_equal 29, last_response.headers['X-RateLimit-Remaining']
-      assert_in_delta (Time.now + 2 * 60 * 60).to_i, last_response.headers['X-RateLimit-Reset'], 5
+      assert_equal 29, last_response.headers['X-RateLimit-Remaining'].to_i
+      assert_in_delta (Time.now + 2 * 60 * 60).to_i, last_response.headers['X-RateLimit-Reset'].to_i, 5
     end
   end
 
   def test_middleware_separates_limit_for_each_client
     5.times { get '/', {}, 'REMOTE_ADDR' => '10.0.0.1' }
 
-    assert_equal 25, last_response.headers['X-RateLimit-Remaining']
+    assert_equal 25, last_response.headers['X-RateLimit-Remaining'].to_i
 
     10.times { get '/', {}, 'REMOTE_ADDR' => '10.0.0.2' }
 
-    assert_equal 20, last_response.headers['X-RateLimit-Remaining']
+    assert_equal 20, last_response.headers['X-RateLimit-Remaining'].to_i
   end
 
   def test_middleware_handles_configuration_block
@@ -98,11 +98,11 @@ class RateLimiterTest < Minitest::Test
 
     25.times { get '/', { 'api_token' => 12345 } }
 
-    assert_equal 5, last_response.headers['X-RateLimit-Remaining']
+    assert_equal 5, last_response.headers['X-RateLimit-Remaining'].to_i
 
     5.times { get '/', { 'api_token' => 54321 } }
 
-    assert_equal 25, last_response.headers['X-RateLimit-Remaining']
+    assert_equal 25, last_response.headers['X-RateLimit-Remaining'].to_i
 
     60.times { get '/', {} }
 
